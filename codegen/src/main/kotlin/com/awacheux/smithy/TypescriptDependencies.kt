b4eeb8e9b66a30fdd7b4a6046ff7b276574d7483
Antoine Wacheux <awacheux.utc@gmail.com>
2023-12-24 11:20:48 +0100

    Add the Zod dependency

---
+++ codegen/src/main/kotlin/com/awacheux/smithy/TypescriptDependencies.kt
@@ -0,0 +1,27 @@
+package com.awacheux.smithy
+
+import software.amazon.smithy.codegen.core.SymbolDependency
+import software.amazon.smithy.codegen.core.SymbolDependencyContainer
+
+enum class TypescriptDependencies(dependencyType: String, packageName: String, version: String): SymbolDependencyContainer {
+
+    ZOD("dependencies", "zod", "^3.22.4");
+
+    private val dependency: SymbolDependency =
+        SymbolDependency.builder().dependencyType(dependencyType).packageName(packageName).version(version).build()
+
+    /**
+     * Gets the list of dependencies that this object introduces.
+     *
+     *
+     * A dependency is a dependency on another package that a Symbol
+     * or type requires. It is quite different from a reference since a
+     * reference only refers to a symbol; a reference provides no context
+     * as to whether or not a dependency is required or the dependency's
+     * coordinates.
+     *
+     * @return Returns the dependencies.
+     */
+    override fun getDependencies(): MutableList<SymbolDependency> = mutableListOf(dependency)
+
+}
